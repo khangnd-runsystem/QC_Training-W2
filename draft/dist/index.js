a3ed48e3f0470464db942f4c8e20bf25a3fb8c57
khangnd-runsystem <khangnd@runsystem.net>
2025-10-08 10:36:36 +0700

    choir(draft): testing trivial logic with TypeScript in draft folder

---
+++ draft/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    continue;
+    console.log('unreachable');
+}
+//# sourceMappingURL=index.js.map
